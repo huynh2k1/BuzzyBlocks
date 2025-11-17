16427878eff5622211d74fd6035c8f16880ed6f4
DESKTOP-0F04UBV\huynh <Huynh122001@gmail.com>
2025-11-17 21:56:23 +0700

    update

--- Assets/Scripts/Gameplay/HexaCell.cs
+++ Assets/Scripts/Gameplay/HexaCell.cs
@@ -1,8 +1,45 @@
+using TMPro;
 using UnityEngine;
 
 public class HexaCell : MonoBehaviour
 {
+    public int q;
+    public int r;
+
+    public int quantityUnlock;
+
     public CellType cellType;
+
+    [SerializeField] TMP_Text txtNumUnlock;
+
+    [SerializeField] GameObject lockObj;
+
+    void UpdateTxtNumUnlock(int quantity)
+    {
+        txtNumUnlock.text = quantity.ToString();
+    }
+
+    void CheckQuantityRemaining(int number)
+    {
+        if(number >= quantityUnlock)
+        {
+            //Unlock
+            ShowLock(false);
+        }
+        else
+        {
+            //Lock
+        }
+    }
+
+    void ShowLock(bool isShow, int quantity = 0)
+    {
+        if (isShow)
+        {
+            UpdateTxtNumUnlock(quantity);
+        }
+        lockObj.SetActive(isShow);  
+    }
 }
 public enum CellType
 {
