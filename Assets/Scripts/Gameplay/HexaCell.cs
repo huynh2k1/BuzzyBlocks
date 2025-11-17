d89f7fd31fae0fa6b48cc055dccff52d8809d181
Huynh <huynhnv@solarapp.asia>
2025-11-17 17:27:51 +0700

    update

--- Assets/Scripts/Gameplay/HexaCell.cs
+++ Assets/Scripts/Gameplay/HexaCell.cs
@@ -2,21 +2,14 @@ using UnityEngine;
 
 public class HexaCell : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public CellType cellType;
 }
 public enum CellType
 {
     EMPTY,
     NORMAL,
-    LOCK,
+    LOCK10,
+    LOCK20,
+    LOCK30,
+    LOCK50,
 }
